3ec7df1c1410e5cdc496e6a71b35b7f97640bea6
Jeisson Montenegro <soyjeison2017@gmail.com>
2023-04-21 09:49:00 -0500

    models

---
+++ modelos/usuario.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose')
+
+const UserSchema = Schema({
+  name: {
+    type: String,
+    require: [true, 'Name is required']
+  },
+  email: {
+    type: String,
+    require: [true, 'Email is required'],
+    unique: true
+  },
+  password: {
+    type: String,
+    require: [true, 'Password is required'],
+  },
+  img: {
+    type: String,
+  },
+  role: {
+    type: String,
+    require: true,
+    emun: ['ADMIN_ROLE', 'USER_ROLE']
+  },
+  state: {
+    type: Boolean,
+    default: true
+  },
+  google: {
+    type: Boolean,
+    default: false
+  }
+})
+
+
+UserSchema.methods.toJSON = function () {
+  const { __v, password, ...usuario } = this.toObject();
+  return usuario
+}
+
+module.exports = model('Usuario', UserSchema);
